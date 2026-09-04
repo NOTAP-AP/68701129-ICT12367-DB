25a58be282997ef2316d108699f993f055cdcbd3
NOTAP-AP <pwaas600@gmail.com>
2026-09-04 11:24:13 +0700

    Initial commit

--- src/router/index.js
+++ src/router/index.js
@@ -10,10 +10,20 @@ const routes = [
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+    
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/customer',
+    name: 'customer',
+    
+    component: () => import(/* webpackChunkName: "about" */ '../views/CustomerView.vue')
+  },
+  {
+    path: '/employee',
+    name: 'employee',
+    
+    component: () => import(/* webpackChunkName: "about" */ '../views/EmployeeView.vue')
   }
 ]
 
